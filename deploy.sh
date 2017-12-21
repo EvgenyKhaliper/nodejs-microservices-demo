52eb302bccd343f4b5000dc8f4c68bc2b350501e
Evgeny Khaliper <eugen.none@gmail.com>
2017-12-21 13:43:22 +0200

    done

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+ kubectl create -f ~/Work/nodejs-microservices-demo/k8s/blog-namespace.yaml;
+ kubectl create -f ~/Work/nodejs-microservices-demo/k8s/deployments/;
+ kubectl create -f ~/Work/nodejs-microservices-demo/k8s/services/;
